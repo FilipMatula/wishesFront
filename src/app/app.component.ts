d35c530cd3b5b2c6507c3fc296343281d778d4b3
Filip Matula <filip.matula@o2.pl>
2023-05-24 20:36:02 +0200

    - i think ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,48 @@
-import { Component } from '@angular/core';
-import { ViewType } from './types';
+import { Component, OnInit } from '@angular/core';
+import { ViewType, Wish } from './types';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.newWish = {
+      signature: '',
+      wishes: '',
+      url: null
+    }
+  }
+
   currentPage: ViewType = "Add";
-  wishes: string = '';
-  signature: string = '';
-  url: any = null;
+  newWish: Wish;
   submitted: boolean = false;
+  wishes: Wish[] = [];
 
   public goTo(type: ViewType): void {
     this.currentPage = type;
-    console.log(this.currentPage);
   }
 
   public clear(): void {
-    this.wishes = '';
-    this.signature = '';
+    this.newWish.wishes = '';
+    this.newWish.signature = '';
+    this.newWish.url = null;
+    this.submitted = false;
   }
 
   public preview(): void {
     this.submitted = true;
-    if (this.wishes !== '' && this.signature !== '') {
+    if (this.newWish.wishes !== '' && this.newWish.signature !== '') {
       this.goTo('Preview');
     }
   }
 
   public send(): void {
-
+    this.wishes.push({...this.newWish});
+    console.log(this.wishes);
+    this.clear();
+    this.goTo('ViewAll');
   }
 
   public onSelectFile(event: any): void {
@@ -40,7 +51,7 @@ export class AppComponent {
       var reader = new FileReader();
       reader.readAsDataURL(file);
       reader.onload = (event) => {
-        this.url = (<FileReader>event.target).result;
+        this.newWish.url = (<FileReader>event.target).result;
       }
     }
   }
--- src/app/types.ts
+++ src/app/types.ts
@@ -1,1 +1,7 @@
 export type ViewType = 'Add' | 'Preview' | 'ViewAll'
+
+export interface Wish {
+    wishes: string;
+    signature: string;
+    url: any;
+}
